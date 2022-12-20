08ca4c5b04a7a19864f5163cc70631fa69866c30
mason0713h <81177466+mason0713h@users.noreply.github.com>
2022-12-20 11:56:12 +0900

    Update and rename git push.txt to git push.c

---
+++ git push.c
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int correct = 1;
+	while (correct <= 10)
+	{
+		int first = rand() % 20, second = rand() % 20, c = rand() % 4, input, ans;
+		char ch;
+		printf("[%d/10] ", correct);
+		switch (c)
+		{
+		case 0:
+			ch = '+';
+			ans = (first + 1) + (second + 1);
+			break;
+		case 1:
+			ch = '-';
+			ans = (first + 1) - (second + 1);
+			break;
+		case 2:
+			ch = '*';
+			ans = (first + 1) * (second + 1);
+			break;
+		case 3:
+			ch = '/';
+			ans = (first + 1) / (second + 1);
+			break;
+		default:
+			break;
+		}
+		printf("%d %c %d = ", (first + 1), ch, (second + 1));
+		scanf("%d", &input);
+		if (ans == input)
+		{
+			printf("\nCorrect!\n\n");
+			correct++;
+		}
+		else printf("\nWrong! Answer is %d.\n\n", ans);
+	}
+}
